2d3bd98f0cc6495e484029f7f9e2e5db62194560
sbracely <sbrace0822@gmail.com>
2026-05-15 12:14:50 +0800

    init

---
+++ src/main/java/io/github/sbracely/samplespringkafkamicroservices/SampleSpringKafkaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package io.github.sbracely.samplespringkafkamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringKafkaMicroservicesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SampleSpringKafkaMicroservicesApplication.class, args);
+    }
+
+}
